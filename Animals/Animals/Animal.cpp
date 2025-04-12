22828d71bce535dea793571439de418184880b12
bilyana bilyanova <bilyana03b@gmail.com>
2025-04-12 15:45:11 +0300

    #Почти готов class Animal

--- Animals/Animals/Animal.cpp
+++ Animals/Animals/Animal.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <string.h>
 #include "Animal.h"
+
+Animal::Animal() {
+	name = new char[1];
+	name[0] = '\0';
+	movementSpeed = 0;
+	foodSupplyPerMeal = 0; 
+	foodArea = 0;
+	age = 0;
+}
+
+Animal::Animal(char* pName, int speed, int foodPerMeal, int eatingArea, int years) {
+	name = new char[strlen(pName)];			// може да потрябва +1 за '\0', но не съм сигурна, засега ще е така
+	strcpy_s(name, strlen(pName), pName);
+
+	movementSpeed = speed;
+	foodSupplyPerMeal = foodPerMeal;
+	foodArea = eatingArea;
+	age = years;
+}
+
+Animal::Animal(const Animal &animal) {
+	int lenght = strlen(animal.name) + 1; // +1 за '\0' май ще потрябва
+	name = new char[lenght];
+	strcpy_s(name, lenght, animal.name); // може да потрябва this->name, засега ще е така
+	movementSpeed = animal.movementSpeed;
+	foodSupplyPerMeal = animal.foodSupplyPerMeal;
+	foodArea = animal.foodArea;
+	age = animal.age;
+}
+
+Animal::~Animal() {
+	delete[] name;
+}
+
+void Animal::Feed() {
+	// ??????????????????????????????
+}
+
+void Animal::PrintInfo() {
+	std::cout << "Animal name: " << name << std::endl;
+	std::cout << "Animal movement speed: " << movementSpeed << std::endl;
+	std::cout << "Animal food supply per meal: " << foodSupplyPerMeal << std::endl;
+	std::cout << "Animal food area (in meters): " << foodArea << std::endl;
+	std::cout << "Animal age: " << age << std::endl;
+}
--- Animals/Animals/Animal.h
+++ Animals/Animals/Animal.h
@@ -1,18 +1,19 @@
 #pragma once
 class Animal
 {
-	char name[20];
-	int mouvementSpeed;
-	int foodSupplyForOneMeal;
+protected:
+	char *name;																				// с динамична памет
+	int movementSpeed;
+	int foodSupplyPerMeal;
 	int foodArea;
 	int age;
 
 public:
-	Animal(char* name, int mouvementSpeed, int foodSupplyForOneMeal, int foodArea, int age);
-	Animal();
-	~Animal();
-	void printInfo();
-	void feading();
-	
+	Animal();																				// default constructor
+	Animal(char* name, int movementSpeed, int foodSupplyPerMeal, int foodArea, int age);	// constructor с параметри
+	Animal(const Animal &animal);															// copy constructor
+	~Animal();																				// destrucor
+	virtual void Feed();																	// ще се наследява и ще е overriden, затова virtual
+	virtual void PrintInfo();																// ще се наследява и ще е overriden, затова virtual
 };
 
