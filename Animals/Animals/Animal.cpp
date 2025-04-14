9c29a5290bb3adca9f49018a68d6b3728edfe75d
bilyana bilyanova <bilyana03b@gmail.com>
2025-04-14 19:18:54 +0300

    Добавяне на нови променливи

--- Animals/Animals/Animal.cpp
+++ Animals/Animals/Animal.cpp
@@ -5,16 +5,22 @@
 Animal::Animal() {
 	name = new char[1];
 	name[0] = '\0';
+	type = 0;
+	hungry = 0;
 	movementSpeed = 0;
 	foodSupplyPerMeal = 0; 
 	foodArea = 0;
 	age = 0;
 }
 
-Animal::Animal(char* pName, int speed, int foodPerMeal, int eatingArea, int years) {
+Animal::Animal(const TPoint Placement, char* pName, int species, int fed, int speed, int foodPerMeal, int eatingArea, int years) {
 	name = new char[strlen(pName)];			// може да потрябва +1 за '\0', но не съм сигурна, засега ще е така
 	strcpy_s(name, strlen(pName), pName);
+	
+	Position = Placement;
 
+	type = species;
+	hungry = fed;
 	movementSpeed = speed;
 	foodSupplyPerMeal = foodPerMeal;
 	foodArea = eatingArea;
@@ -25,6 +31,11 @@ Animal::Animal(const Animal &animal) {
 	int lenght = strlen(animal.name) + 1; // +1 за '\0' май ще потрябва
 	name = new char[lenght];
 	strcpy_s(name, lenght, animal.name); // може да потрябва this->name, засега ще е така
+
+	Position = animal.Position;
+
+	type = animal.type;
+	hungry = animal.hungry;
 	movementSpeed = animal.movementSpeed;
 	foodSupplyPerMeal = animal.foodSupplyPerMeal;
 	foodArea = animal.foodArea;
@@ -35,12 +46,28 @@ Animal::~Animal() {
 	delete[] name;
 }
 
-void Animal::Feed() {
-	// ??????????????????????????????
+void Animal::Eat() {
+	if (type == 1) {
+		for (int i = 0; i < P->Table[i]->Position.x > Position.x - foodArea && i < P->Table[i]->Position.x < Position.x + foodArea &&
+			i < P->Table[i]->Position.y > Position.y - foodArea && i < P->Table[i]->Position.y < Position.y + foodArea; i++) {
+			// ??????????????????????????????????????
+		}
+	}
+	
 }
 
 void Animal::PrintInfo() {
 	std::cout << "Animal name: " << name << std::endl;
+
+	std::cout << "Animal - Position: (" << Position.x << ", " << Position.y << ")" << std::endl;
+
+	if (type == 1) std::cout << "Animal type: Predator" << std::endl;
+	else std::cout << "Animal type: Herbivore" << std::endl;
+
+	if (hungry == 1) std::cout << "Hungry: NO" << std::endl;
+	else std::cout << "Hungry: YES" << std::endl;
+
+	std::cout << "Animal movement speed: " << movementSpeed << std::endl;
 	std::cout << "Animal movement speed: " << movementSpeed << std::endl;
 	std::cout << "Animal food supply per meal: " << foodSupplyPerMeal << std::endl;
 	std::cout << "Animal food area (in meters): " << foodArea << std::endl;
--- Animals/Animals/Animal.h
+++ Animals/Animals/Animal.h
@@ -1,8 +1,17 @@
 #pragma once
+
+struct TPoint {
+	int x;
+	int y;
+};
+
 class Animal
 {
 protected:
+	TPoint Position;
 	char *name;																				// с динамична памет
+	int type;																				// 0 за тревопасно и 1 за хищник
+	int hungry;																				// 0 за гладен и 1 за сит
 	int movementSpeed;
 	int foodSupplyPerMeal;
 	int foodArea;
@@ -10,10 +19,10 @@ protected:
 
 public:
 	Animal();																				// default constructor
-	Animal(char* name, int movementSpeed, int foodSupplyPerMeal, int foodArea, int age);	// constructor с параметри
+	Animal(const TPoint Position, char* name, int type, int fed, int movementSpeed, int foodSupplyPerMeal, int foodArea, int age);	// constructor с параметри
 	Animal(const Animal &animal);															// copy constructor
 	~Animal();																				// destrucor
-	virtual void Feed();																	// ще се наследява и ще е overriden, затова virtual
+	virtual void Eat();																		// ще се наследява и ще е overriden, затова virtual
 	virtual void PrintInfo();																// ще се наследява и ще е overriden, затова virtual
 };
 
