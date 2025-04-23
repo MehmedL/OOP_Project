b29a656ac69f66e8363cac21a84b53fb9d2bab56
Gyokan0 <gyokan03@gmail.com>
2025-04-23 15:04:34 +0300

    Prey.cpp файла ето
    само някой да провери в края когато всичко е готов да ли е вярно

--- Animals/Animals/Prey.cpp
+++ Animals/Animals/Prey.cpp
@@ -1,1 +1,14 @@
-#include "Prey.h"
+#pragma once
+#include "Animal.h"
+
+class Prey : public Animal
+{
+    char deffenseWay[20];
+
+public:
+    Prey();
+    Prey(const TPoint Position, char* name, int fed, int movementSpeed, int foodSupplyPerMeal, int foodArea, int age, const char* deffenseWay);
+    ~Prey();
+    void Eat() override;
+    void PrintInfo() override;
+};
