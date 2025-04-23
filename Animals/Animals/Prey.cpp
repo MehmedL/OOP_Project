d6073eadf9aa5553ca00192262c9e49e600c1d50
Gyokan0 <gyokan03@gmail.com>
2025-04-23 15:05:18 +0300

    Prey.cpp

--- Animals/Animals/Prey.cpp
+++ Animals/Animals/Prey.cpp
@@ -1,14 +1,37 @@
-#pragma once
-#include "Animal.h"
+#include "Prey.h"
+#include <iostream>
+#include <string.h>
 
-class Prey : public Animal
-{
-    char deffenseWay[20];
+Prey::Prey() : Animal() {
+    strcpy_s(deffenseWay, "None");
+}
 
-public:
-    Prey();
-    Prey(const TPoint Position, char* name, int fed, int movementSpeed, int foodSupplyPerMeal, int foodArea, int age, const char* deffenseWay);
-    ~Prey();
-    void Eat() override;
-    void PrintInfo() override;
-};
+Prey::Prey(const TPoint Position, char* pName, int fed, int speed, int foodPerMeal, int eatingArea, int years, const char* defWay)
+    : Animal(Position, pName, 0, fed, speed, foodPerMeal, eatingArea, years) {
+    strcpy_s(deffenseWay, defWay);
+}
+
+Prey::~Prey() {
+    // Няма нужда от специално изчистване
+}
+
+void Prey::Eat() {
+    if (hungry == 0 && foodSupplyPerMeal > 0) {
+        foodSupplyPerMeal--;
+        if (foodSupplyPerMeal == 0)
+            hungry = 1;
+    }
+}
+
+void Prey::PrintInfo() {
+    std::cout << "Animal name: " << name << std::endl;
+    std::cout << "Animal - Position: (" << Position.x << ", " << Position.y << ")" << std::endl;
+    std::cout << "Animal type: Herbivore" << std::endl;
+    std::cout << "Hungry: " << (hungry ? "NO" : "YES") << std::endl;
+    if (!hungry)
+        std::cout << "Meals needed to be full: " << foodSupplyPerMeal << std::endl;
+    std::cout << "Animal movement speed: " << movementSpeed << std::endl;
+    std::cout << "Animal food area (in meters): " << foodArea << std::endl;
+    std::cout << "Animal age: " << age << std::endl;
+    std::cout << "Defense way: " << deffenseWay << std::endl;
+}
